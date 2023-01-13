2011e73a8cff9493e60c17205e61d4774e205bbe
Nada Ashraf Mahmoud <122626045+NadaAshraf12@users.noreply.github.com>
2023-01-13 08:13:34 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double RectangleLength, RectangleWidth, Area;
+    cout << "Enter Rectangle Length and Rectangle Width  \n";
+    cin>>RectangleLength>>RectangleWidth;
+    Area=RectangleLength*RectangleWidth;
+    cout<<"The Area of the Rectangle="  << Area  <<"\n";
+
+
+
+    return 0;
+}
